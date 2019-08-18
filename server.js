5dfda228657b10dbe85587ed39eb0f721758f309
Justin Yarbrough <justin.yarbrough@twisted-rope.com>
2019-08-18 15:32:27 -0400

    Add dotenv settings, uuid, and prop-types; update dependencies

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './settings.env' });
+
+const HOST = process.env.CHAT_HOST;
+const PORT = process.env.CHAT_PORT;
 
 io.on('connection', socket => {
   
   console.log('A user connected...');
 
   socket.on('chat message', msg => {
-    // console.log(msg);
     io.emit('chat message', msg);
   });
 });
 
-http.listen(4000, function(){
-  console.log('listening on *:4000');
+io.on('disconnect', socket => {
+  console.log('User has disconnected.');
+});
+
+http.listen(PORT, function() {
+  console.log(`Listening on ${HOST}:${PORT}`);
 });
